8dcdfdf967736b0a5dbeca79204f5cce3a9d58e4
Pitambar0978 <gd2066376@gmail.com>
2026-04-11 14:51:30 +0530

    first commit

---
+++ JS/index.js
@@ -0,0 +1,19 @@
+setTimeout(function(){
+    document.getElementsByClassName("box")[0].style.display="block";
+},1000);
+
+setTimeout(function(){
+    document.getElementsByClassName("aboutnotice")[0].style.display="flex";
+},2000);
+
+setTimeout(function(){
+    document.getElementsByClassName("people")[0].style.display="flex";
+},3000);
+
+setTimeout(function(){
+    document.getElementsByClassName("infrastructure")[0].style.display="block";
+},4000);
+
+setTimeout(function(){
+    document.getElementsByClassName("foot")[0].style.display="flex";
+},5000);
---
+++ JS/loader.js
@@ -0,0 +1,3 @@
+setTimeout(() => {
+    window.location.href = "/HTML/main.html";
+},5000);
