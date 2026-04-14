bba80fa68d957a2e72fb6daf1bb7148ff0643712
Pitambar0978 <gd2066376@gmail.com>
2026-04-14 10:16:16 +0530

    gn

--- JS/index.js
+++ JS/index.js
@@ -1,11 +1,15 @@
 setTimeout(function(){
     document.getElementsByClassName("box")[0].style.display="block";
 },1000);
-
 setTimeout(function(){
     document.getElementsByClassName("aboutnotice")[0].style.display="flex";
+},1000);
+setTimeout(function(){
+    document.getElementById("about").style.display="block";
 },2000);
-
+setTimeout(function(){
+    document.getElementById("notice").style.display="block";
+},3000);
 setTimeout(function(){
     document.getElementsByClassName("people")[0].style.display="flex";
 },3000);
